656b3d5825c8d1b74d175dcca9438844536ac8d4
KOLANICH <kolan_n@mail.ru>
2023-10-17 22:26:05 +0300

    We had to move to Codeberg

--- JImageHash/__init__.py
+++ JImageHash/__init__.py
@@ -1,5 +1,8 @@
 from enum import Enum
 from pathlib import Path
+from warnings import warn
+
+warn("We have moved from M$ GitHub to https://codeberg.org/KOLANICH-libs/JImageHash.py , read why on https://codeberg.org/KOLANICH/Fuck-GuanTEEnomo .")
 
 import JAbs
 import numpy as np
